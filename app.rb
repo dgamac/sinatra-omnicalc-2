12b388488f471d48d572fa186ee0d03b3fc6e1d6
dgamac <165616196+dgamac@users.noreply.github.com>
2024-05-07 15:06:24 +0000

    fixed routes

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ get("/") do
   redirect("/add")
 end
 
-get("/ad") do
+get("/add") do
   erb(:add_form)
 end
 
@@ -28,7 +28,7 @@ get("/wizard_subtract") do
 end
 
 get("/multiply") do
-  erb(:multiplication_form)
+  erb(:mult_form)
 end
 
 get("/wizard multiply") do

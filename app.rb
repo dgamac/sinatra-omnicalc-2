8032dd1e4c27f779065c0683fa82ff2777099877
dgamac <165616196+dgamac@users.noreply.github.com>
2024-05-07 16:02:27 +0000

    fixed mult

--- app.rb
+++ app.rb
@@ -24,9 +24,8 @@ end
 get("/wizard_subtract") do
   @first_num = params.fetch("first_number").to_f
   @second_num = params.fetch("second_number").to_f
-  
-  @result = @second_num - @first_num
 
+  @result = @second_num - @first_num
   erb(:sub_result)
 end
 
@@ -37,9 +36,7 @@ end
 get("/wizard_multiply") do
   @first_num = params.fetch("first_num").to_f
   @second_num = params.fetch("second_num").to_f
-
   @result = @first_num * @second_num
-
   erb(:mult_result)
 end
 
@@ -48,9 +45,8 @@ get("/divide") do
 end
 
 get("/wizard_divide") do
-  @first_num = params.fetch("first_num").to_f
-  @second_num = params.fetch("second_num").to_f
-
+  @first_num = params.fetch("first_number").to_f
+  @second_num = params.fetch("second_number").to_f
   @result = @first_num / @second_num
   erb(:div_result)
 end
